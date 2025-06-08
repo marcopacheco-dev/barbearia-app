981d2022570b5736272fab7ab98ab5ac6c7e0882
Marco Pacheco <marcopacheco.dev@hotmail.com>
2025-06-08 12:45:04 -0300

    Ajustando o frontend para ter acesso a API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './services/auth.interceptor';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSnackBarModule,
+    MatSnackBarModule
   ],
   providers: [
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideHttpClient(withInterceptors([AuthInterceptor])) // aqui no providers
   ],
+  bootstrap: [/* seu componente raiz, ex: AppComponent */]
 })
 export class AppModule {}
